3444e65e3b0c2a7f295885bd6d01b59a59978687
BolotaevDavid <bolotaev-david@mail.ru>
2019-10-09 23:47:23 +0300

    Update UseDefaults.swift

--- HW_3_Generic_and_Errors/UseDefaults.swift
+++ HW_3_Generic_and_Errors/UseDefaults.swift
@@ -18,20 +18,20 @@ enum CustomError: Error {
 
 struct Persistant<T> {
     
-    let userDefaults = UserDefaults.standard
+    weak var userDefaults = UserDefaults.standard
     let error = CustomError.error
     
     func write(_ value: T, forKey: String) {
-        userDefaults.set(value, forKey: forKey)
+        userDefaults?.set(value, forKey: forKey)
     }
     
-    func reade(_ forKey: String) throws -> Result<String, Error>{
-        guard let returnValue = userDefaults.object(forKey: forKey) else {
+    func reade(_ forKey: String) -> Result<T, Error>{
+        guard let returnValue = userDefaults?.object(forKey: forKey) as? T else {
             print(error.localizedDescription)
             return .failure(error)
         }
         print("We got some value = \(returnValue)")
-        return .success("We got some value = \(returnValue)")
+        return .success(returnValue)
     }
     
 }
